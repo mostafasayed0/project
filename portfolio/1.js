4efed32df64edb6b424fb54f773b464adaad7dc8
mostafasayed0 <88598128+mostafasayed0@users.noreply.github.com>
2022-01-03 15:13:51 +0200

    Add files via upload

---
+++ portfolio/1.js
@@ -0,0 +1,42 @@
+var name = window.prompt("Enter vistor Name");
+window.alert("hello sir "+name)
+
+// var num1= Number(window.prompt("Enter num1")) ;
+// var num2= Number(window.prompt("Enter num2")) ;
+// window.alert("the sum of two number is = "+(num1+num2))
+
+
+
+
+
+
+
+// +
+// contanction operator
+// فى الجافا دى لدله جمع وبرضوا اداه ربط
+
+
+// console.log(5+5)   10          نوعها       int
+// console.log(5+"5")    55       نوعها      string
+// console.log("5"+"5") 55        نوعها     string
+// console.log(5+5+"5")    105              string
+// console.log("5"+5+5)     555             string
+// بتقرا من الشمال لليمين
+// string يكسب
+
+
+
+// Number("1000")   10000
+
+// console.log(Number(""))   0
+// console.log(Number(" "))  0
+// console.log(Number("false"))    0
+
+// console.log(Number("True"))      1
+
+// console.log(Number("mostafa"))   دى هتطلع not a number  (Nan) يعنى مقدرش يحولها لرقم
+
+
+
+// implicit converstion 
+// console.log(5 * "5")      25
